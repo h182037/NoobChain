215e784605c3f5836c0237b41d49b7168ef2a68a
h182037 <sindre.aalhus@gmail.com>
2018-10-24 13:15:41 +0200

    added createTransaction

--- src/no/hvl/dat159/Wallet.java
+++ src/no/hvl/dat159/Wallet.java
@@ -2,6 +2,7 @@ package no.hvl.dat159;
 
 import java.security.KeyPair;
 import java.security.PublicKey;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
@@ -9,7 +10,7 @@ public class Wallet {
 
     private String id;
     private KeyPair keyPair;
-    
+    private long newTxOutputs;
     //A refererence to the "global" complete utxo-set
     private Map<Input, Output> utxoMap;
     
@@ -35,15 +36,29 @@ public class Wallet {
     	long myBalance = calculateBalance(myUTXO.values());
         // 2. Check if there are sufficient funds --- Exception?
     	if(myBalance < value) System.out.println("Poor man is poor");
-  
-        // 3. Choose a number of UTXO to be spent --- Strategy?
     	
+        // 3. Choose a number of UTXO to be spent --- Strategy?
+    	newTxOutputs = 0;
+    	ArrayList<Input> chosenTxs = new ArrayList<Input>();
+        while(newTxOutputs < value) {
+        	myUTXO.forEach((input, output) -> {
+        		newTxOutputs += output.getValue();
+        		chosenTxs.add(input);
+        	});
+        }
+
         // 4. Calculate change
+        long change = newTxOutputs - value;
+        
         // 5. Create an "empty" transaction
     	Transaction tx = new Transaction(keyPair.getPublic());
         // 6. Add chosen inputs
-    	
+    	chosenTxs.forEach((input)-> {
+    		tx.addInput(input);
+    	});
         // 7. Add 1 or 2 outputs, depending on change
+    	tx.addOutput(new Output(value, address));
+    	if(change > 0) tx.addOutput(new Output(change, getAddress()));
         // 8. Sign the transaction
     	tx.signTxUsing(keyPair.getPrivate());
         // 9. Calculate the hash for the transaction
